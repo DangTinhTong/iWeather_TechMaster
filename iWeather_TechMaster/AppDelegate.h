abebef8a38791328d69d7cf5cbbf31ae171340a1
admin <dangtinhtong@gmail.com>
2017-10-23 00:19:55 +0700

    Initial Commit

---
+++ iWeather_TechMaster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iWeather_TechMaster
+//
+//  Created by admin on 10/23/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
