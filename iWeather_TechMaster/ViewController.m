1506bf9baa00747d7595cefb06bd1fd5ae0ce2f3
admin <dangtinhtong@gmail.com>
2017-11-03 23:52:15 +0700

    * Have to try, try, try......

--- iWeather_TechMaster/ViewController.m
+++ iWeather_TechMaster/ViewController.m
@@ -17,16 +17,36 @@
 @end
 
 @implementation ViewController
+// Khai báo các phân tử
+{
+    NSArray* quotes;
+    NSArray* locations;
+    NSArray* photoFiles;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    quotes =@[@"Trên con đường thành công không có dấu chân của kẻ lười biếng",@"Một con ngựa đau cả tàu bỏ cỏ",@"Đừng so sanh mình với bất cứ ai trong thế giới này. Nếu làm như vậy có nghĩa là bạn đang sỉ nhục bản thân",@"Cuộc sống không công bằng. Hãy tập quen dần với điều đó",@"Ta không chonj được nơi sinh ra mình. Nhưng ta chọn cách mình sẽ sống" ];
+    locations =@[@" Hà Nội, Việt Nam",@"Jakarta, Indonesia",@"Naypyidaw, Myanmar",@"Bangkok, Thái Lan"];
+    photoFiles =@[@"Rains",@"Thunders",@"Windies",@"Sunnies"];
 }
+
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)UpdateIweather:(id)sender {
+   unsigned int quoteIndex=arc4random_uniform(quotes.count);
+    self.Quotes.text=quotes[quoteIndex];
+    
+    unsigned int loctionsIndex=arc4random_uniform(locations.count);
+    self.Locations.text=locations[loctionsIndex];
+    
+    unsigned int photoIndex=arc4random_uniform(photoFiles.count);
+    self.statusWeather.image =[UIImage imageNamed:photoFiles[photoIndex]];
 }
 
 
